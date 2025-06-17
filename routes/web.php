b67ed8a6ceb3442b66b18c4154573b421c54ca43
Zaber30 <alzaberalzaber23@gmail.com>
2025-06-17 21:58:44 +0600

    out

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/out',function(){
+    return'zaber how are you';
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
